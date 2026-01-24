b1240e0ff604535982c995d48f35c21c4d9e7214
davydKuleba <dkuleba@air.io>
2026-01-24 13:26:54 -0500

    clean up project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,7 @@ export default async function Home() {
 
   return (
     <div className="app-container">
-      <div className="main-card-wrapper">
-        <img
-          src="https://cdn.auth0.com/quantum-assets/dist/latest/logos/auth0/auth0-lockup-en-ondark.png"
-          alt="Auth0 Logo"
-          className="auth0-logo"
-        />
-        <h1 className="main-title">Next.js + Auth0</h1>
-        
+      <div className="main-card-wrapper">        
         <div className="action-card">
           {user ? (
             <div className="logged-in-section">
@@ -27,9 +20,6 @@ export default async function Home() {
             </div>
           ) : (
             <>
-              <p className="action-text">
-                Welcome! Please log in to access your protected content.
-              </p>
               <LoginButton />
             </>
           )}
