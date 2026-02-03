efbd36a370d35302a538bf0f1eb8848dabdad87f
Arsenii Buriak <128735775+SenchaBka@users.noreply.github.com>
2026-02-02 21:05:38 -0500

    Browser icon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/icons/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link 
